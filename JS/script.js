506aaadd1e079034146ee8ce8476250ab2382919
Matthew Davoren <matthewdavoren@gmail.com>
2024-04-16 18:55:04 -0700

    Making website reponsive for mobile and tablet

--- JS/script.js
+++ JS/script.js
@@ -24,6 +24,24 @@ function toggleMenu() {
     }
   }
 
+  function toggleHamburger() {
+    var hamburgerList = document.querySelector('.hamburger-list');
+    
+    if (hamburgerList.style.transform === 'translateX(0px)') {
+      hamburgerList.style.transform = 'translateX(100%)'; // Move out of view
+    } else {
+      hamburgerList.style.display = 'block'; // Ensure it's block before sliding in
+      hamburgerList.style.transform = 'translateX(0px)'; // Slide into view
+    }
+}
+
+document.querySelector('.close-button').addEventListener('click', function() {
+  var hamburgerList = document.querySelector('.hamburger-list');
+  var hamburgerButton = document.querySelector('.hamburger-button');
+  hamburgerList.style.transform = 'translateX(100%)'; // Slide out of view
+  hamburgerButton.style.transform = 'translateX(0px)'; // Reset button position
+});
+
 function smoothScroll(target) {
     document.querySelector(target).scrollIntoView({
         behavior: 'smooth'
